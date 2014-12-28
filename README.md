e38bd9a8e92ccce4388c1d07fcef40569b566df3
Julian C. Dunn <jdunn@getchef.com>
2014-12-28 01:29:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Urbandecoder Labs Proxy Autoconfiguration Cookbook
+==================================================
+
+Set up `wpad.dat` and a `wpad.conf` (for Apache to serve the right
+MIME-type)
+
+License and Authors
+-------------------
+
+* License: Apache 2.0
+* Author: Julian Dunn
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'ud-wpad'
+maintainer       'Urbandecoder Labs LLC'
+maintainer_email 'jdunn@aquezada.com'
+license          'Apache 2.0'
+description      'Installs/Configures ud-wpad'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
